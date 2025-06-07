3171a8ebcf5636b8aaaf934ad1e5087e74df52d0
toqaewa <kath.tokaeva@gmail.com>
2025-06-07 08:26:12 +0300

    ui base + clicker function

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
+  const [count, setCount] = useState<number>(0);
+
+  const handleClick = () => {
+    setCount(prevCount => prevCount + 1);
+  };
+
+  const handleReset = () => {
+    setCount(-1);
+  };
+
   return (
-    <div className="App"></div>
+    <div className="app" onClick={handleClick}>
+      <div className="counter-display">
+        <h1>Счетчик кликов</h1>
+        <p>Кликов: {count}</p>
+        <button onClick={handleReset}>Сбросить счетчик</button>
+      </div>
+    </div>
   );
-}
+};
 
 export default App;
