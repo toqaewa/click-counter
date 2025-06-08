14ab7d54800c403c73fac53d3acdc4c79dd1d940
toqaewa <kath.tokaeva@gmail.com>
2025-06-08 09:12:33 +0300

    alert logic

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,22 @@ import './App.css';
 
 const App: React.FC = () => {
   const [count, setCount] = useState<number>(0);
+  const [randCriteria, setRandCriteria] = useState<number>(0);
+  const [alert, setAlert] = useState<boolean>(false);
 
   const handleClick = () => {
+    setAlert(false);
     setCount(prevCount => prevCount + 1);
+    const newCriteria = Math.floor(Math.random() * 11);
+    setRandCriteria(newCriteria);
+    newCriteria > 5 && setAlert(true);
+    console.log(randCriteria, alert);
   };
 
-  const handleReset = () => {
-    setCount(-1);
+  const handleReset = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    setCount(0);
+    setAlert(false);
   };
 
   return (
@@ -18,6 +27,9 @@ const App: React.FC = () => {
         <h1>Счетчик кликов</h1>
         <p>Кликов: {count}</p>
         <button onClick={handleReset}>Сбросить счетчик</button>
+        {
+          alert && <div style={{ color: "red" }}>ALERT!!!</div>
+        }
       </div>
     </div>
   );
